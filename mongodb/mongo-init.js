65ea102f1e1356d3eeda69526eea5769dbe55767
eddacris <eddacris@eddacris-dns.IGD_Rostelecom>
2024-03-12 22:49:02 +0700

    Changed init db

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,25 +1,17 @@
-// db = db.getSiblingDB(process.env.M_DATABASE)
-
-// db.createUser({
-//   user: process.env.M_USERNAME,
-//   pwd: process.env.M_PASSWORD,
-//   roles: [
-//     {
-//       role: 'dbOwner',
-//       db: process.env.M_DATABASE,
-//     },
-//   ],
-// });
-
-db.getSiblingDB('admin').auth(
+db.auth(
   process.env.MONGO_INITDB_ROOT_USERNAME,
   process.env.MONGO_INITDB_ROOT_PASSWORD
 );
+
+db.getSiblingDB(process.env.M_DATABASE);
 
 db.createUser({
   user: process.env.M_USERNAME,
   pwd: process.env.M_PASSWORD,
   roles: [
-    "readWrite"
+    {
+      role: 'root',
+      db: process.env.M_DATABASE,
+    },
   ],
 });
