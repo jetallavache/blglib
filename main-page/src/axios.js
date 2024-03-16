c03ae968ecbbf8375afdbe29985aa2952319d9b6
eddacris <eddacris@eddacris-dns.IGD_Rostelecom>
2024-03-17 02:36:30 +0700

    Test

--- main-page/src/axios.js
+++ main-page/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL_API
-  baseURL: 'https://api.blglib.ru'
+  baseURL: 'http://api.blglib.ru'
 });
 
 export default instance;
