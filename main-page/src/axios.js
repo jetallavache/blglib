f6b7e1e11b5134b34993de0c0f7f9f94c01fc6d8
Николай <jetallavache@outlook.com>
2024-03-19 18:26:43 +0700

    Registration certbot

--- main-page/src/axios.js
+++ main-page/src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL_API
-  baseURL: 'http://api.blglib.ru'
+  // baseURL: process.env.REACT_APP_BASE_URL_API // <---DEV   //НАСТРОИТЬ WEBPACK
+  baseURL: 'http://api.blglib.ru'                // <---PROD
+  // baseURL: 'http://api-blglib:4000'           // ПРОВЕРИТЬ
 });
 
 export default instance;
